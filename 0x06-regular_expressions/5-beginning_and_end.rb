a7d0aed1eedd304b24d2fb2c83514c68536cd446
Wondeman112 <wondem507@gmail.com>
2023-02-02 13:46:58 -0800

    commits

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+#
+
+input = ARGV[0]
+
+
+if input =~ /^h.+n$/
+
+  puts "#{input} matches the pattern."
+
+  else
+
+    puts "#{input} does not match the pattern."
+
+    end
+    
